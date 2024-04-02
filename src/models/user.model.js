8052d9910625567b760253ea36ec7da0a752c2f2
root <root@LAPTOP-J8QUH98P>
2024-04-02 11:21:24 +0530

    first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      match: "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/",
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      maxlength:100,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      maxlength:50,
+      minlength:8
+    },
+    avatar:{
+       type: String //cloudnary
+    },
+    adminUser: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
