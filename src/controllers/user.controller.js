1c93c244113451579ddcf26962f3076f8369a918
Ankur <ankur97206@gmail.com>
2024-04-04 15:56:15 +0530

    remove errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,124 +22,149 @@ const generateAccessAndRefereshTokens = async (userId) => {
 };
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { fullName, email, password } = req.body;
+  try {
+    const { fullName, email, password } = req.body;
 
-  if ([fullName, email, password].some((field) => field?.trim() === "")) {
-    const error = new ApiError(400, "All fields are required");
+    if ([fullName, email, password].some((field) => field?.trim() === "")) {
+      const error = new ApiError(200, "All fields are required");
+      return res
+        .status(209)
+        .json(
+          new ApiResponse(
+            error.statusCode,
+            error.data,
+            "All fields are required"
+          )
+        );
+    }
+
+    const existUser = await User.findOne({
+      email,
+    });
+
+    if (existUser) {
+      const error = new ApiError(209, "User with email already exist");
+      return res
+        .status(209)
+        .json(
+          new ApiResponse(
+            error.statusCode,
+            error.data,
+            "User with email or already exist"
+          )
+        );
+    }
+
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+    });
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken -accessToken "
+    );
+
+    if (!createdUser) {
+      const error = new ApiError(
+        200,
+        " something went wrong while registering"
+      );
+
+      return res
+        .status(209)
+        .json(
+          new ApiResponse(
+            error.statusCode,
+            error.data,
+            "something went wrong while registering"
+          )
+        );
+    }
+
     return res
-      .status(409)
-      .json(
-        new ApiResponse(error.statusCode, error.data, "All fields are required")
-      );
-  }
-
-  const existUser = await User.findOne({
-    email,
-  });
-
-  if (existUser) {
-    const error = new ApiError(409, "User with email already exist");
+      .status(201)
+      .json(new ApiResponse(200, createdUser, "User registered successfully"));
+  } catch (error) {
+    error = new ApiError(200, error?.message);
     return res
-      .status(409)
-      .json(
-        new ApiResponse(
-          error.statusCode,
-          error.data,
-          "User with email or already exist"
-        )
-      );
+      .status(200)
+      .json(new ApiResponse(error.statusCode, error.data, error?.message));
   }
-
-  const user = await User.create({
-    fullName,
-    email,
-    password,
-  });
-  const createdUser = await User.findById(user._id).select("-password ");
-
-  if (!createdUser) {
-    const error = new ApiError(500, " something went wrong while registering");
-
-   return res
-      .status(409)
-      .json(
-        new ApiResponse(
-          error.statusCode,
-          error.data,
-          "something went wrong while registering"
-        )
-      );
-  }
-
-  return res
-    .status(201)
-    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email) {
-    const error = new ApiError(400, "email is requried");
-   return res
-      .status(400)
-      .json(new ApiResponse(error.statusCode, error.data, "email is requried"));
-  }
+    if (!email) {
+      const error = new ApiError(200, "email is requried");
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(error.statusCode, error.data, "email is requried")
+        );
+    }
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    const error = new ApiError(400, "User does not exist");
+    if (!user) {
+      const error = new ApiError(200, "User does not exist");
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(error.statusCode, error.data, "User does not exist")
+        );
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+      const error = new ApiError(200, "Invalid user credentials");
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            error.statusCode,
+            error.data,
+            "Invalid user credentials"
+          )
+        );
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+      user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
     return res
-      .status(400)
-      .json(
-        new ApiResponse(error.statusCode, error.data, "User does not exist")
-      );
-  }
-
-  const isPasswordValid = await user.isPasswordCorrect(password);
-
-  if (!isPasswordValid) {
-    const error = new ApiError(400, "Invalid user credentials");
-    return res
-      .status(400)
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
       .json(
         new ApiResponse(
-          error.statusCode,
-          error.data,
-          "Invalid user credentials"
+          200,
+          {
+            user: loggedInUser,
+            accessToken,
+            refreshToken,
+          },
+          "User logged in successfully"
         )
       );
+  } catch (error) {
+    error = new ApiError(200, error?.message);
+    return res
+      .status(200)
+      .json(new ApiResponse(error.statusCode, error.data, error?.message));
   }
-
-  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
-    user._id
-  );
-
-  const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
-
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-
-  return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
-      new ApiResponse(
-        200,
-        {
-          user: loggedInUser,
-          accessToken,
-          refreshToken,
-        },
-        "User logged in successfully"
-      )
-    );
 });
 
 export { registerUser, loginUser };
